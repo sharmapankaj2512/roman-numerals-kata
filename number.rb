e226a97364e925d3619bd853e033babc80644dd7
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-10-03 06:46:21 +0400

    refactor simplify if statements

--- number.rb
+++ number.rb
@@ -19,28 +19,16 @@ class Roman
   end
 
   def self.to_roman(decimal)
-    if decimal < 10
-      return single_digit(decimal)
-    end
-    if decimal == 10
-      return "X"
-    end
+    return single_digit(decimal) if decimal < 10
+    return "X" if decimal == 10
     return "X" + single_digit(decimal % 10)
   end
 
   def self.single_digit(number)
-    if number < 4
-      return times_one(number)
-    end
-    if number == 4
-      return "IV"
-    end
-    if number < 9
-      return "V" + times_one(number % 5)
-    end
-    if number == 9
-      return "IX"
-    end
+    return times_one(number) if number < 4
+    return "IV" if number == 4
+    return "V" + times_one(number % 5) if number < 9
+    return "IX" if number == 9
   end
 
   def self.times_one(times)
